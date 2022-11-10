675334fc3f1551595db6ca050a734dc6ac09ccee
Ayman Bagabas <ayman.bagabas@gmail.com>
2022-11-10 11:49:46 -0500

    feat(mouse): add extended coordinates and additional buttons support
    The X10 mouse protocol limits the X and Y coordinates to 223 cells only.
    SGR (1006) fixes that.
    
    * Support additional buttons like wheel left/right and backward/forward
    * SGR (1006) mode extended coordinates.
    
    feat(mouse): deprecated MouseEventType and support additional button
    
    This includes wheel left/right, and buttons 8 through 11
    
    chore(mouse): add extended sgr mode tests
    
    chore(mouse): update mouse example
    
    feat(mouse): enable extended mode by default
    
    ref(mouse): rename backward button to back button
    
    fix(mouse): set mouse type to release on SGR
    
    ref(mouse): remove extended pixels mode
    
    fix(mouse): mouse tests

--- standard_renderer.go
+++ standard_renderer.go
@@ -358,6 +358,7 @@ func (r *standardRenderer) enableMouseCellMotion() {
 	defer r.mtx.Unlock()
 
 	r.out.EnableMouseCellMotion()
+	r.out.EnableMouseExtendedMode()
 }
 
 func (r *standardRenderer) disableMouseCellMotion() {
@@ -365,6 +366,7 @@ func (r *standardRenderer) disableMouseCellMotion() {
 	defer r.mtx.Unlock()
 
 	r.out.DisableMouseCellMotion()
+	r.out.DisableMouseExtendedMode()
 }
 
 func (r *standardRenderer) enableMouseAllMotion() {
@@ -372,6 +374,7 @@ func (r *standardRenderer) enableMouseAllMotion() {
 	defer r.mtx.Unlock()
 
 	r.out.EnableMouseAllMotion()
+	r.out.EnableMouseExtendedMode()
 }
 
 func (r *standardRenderer) disableMouseAllMotion() {
@@ -379,6 +382,7 @@ func (r *standardRenderer) disableMouseAllMotion() {
 	defer r.mtx.Unlock()
 
 	r.out.DisableMouseAllMotion()
+	r.out.DisableMouseExtendedMode()
 }
 
 // setIgnoredLines specifies lines not to be touched by the standard Bubble Tea
